94787c0af9bf1995c45b4890f3f76a87663ee4ca
KevinGomes <36061563+KevinGomes@users.noreply.github.com>
2024-05-13 20:26:52 -0400

    updated form + dependencies
    added additional fields to new student form
    
    also changed required dependencies within package.json
    
    also created .gitignore

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -19,7 +19,10 @@ class NewStudentContainer extends Component {
     super(props);
     this.state = {
       firstname: "", 
-      lastname: "", 
+      lastname: "",
+      email: "",
+      imageurl: null,
+      gpa: 0, 
       campusId: null, 
       redirect: false, 
       redirectId: null
@@ -40,6 +43,9 @@ class NewStudentContainer extends Component {
     let student = {
         firstname: this.state.firstname,
         lastname: this.state.lastname,
+        email: this.state.email,
+        imageurl: this.state.imageurl,
+        gpa: this.state.gpa,
         campusId: this.state.campusId
     };
     
@@ -49,7 +55,10 @@ class NewStudentContainer extends Component {
     // Update state, and trigger redirect to show the new student
     this.setState({
       firstname: "", 
-      lastname: "", 
+      lastname: "",
+      email: "",
+      imageurl: null,
+      gpa: 0, 
       campusId: null, 
       redirect: true, 
       redirectId: newStudent.id
